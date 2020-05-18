c9eb1558a4e71b819ba1818c08413e27d310eae3
sudo0272 <sudo0272@gmail.com>
2020-05-19 00:03:50 +0900

    search for meals and show results

--- main_window.py
+++ main_window.py
@@ -1,4 +1,6 @@
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
+import datetime
+from meal_table_item import MealTableItem
 
 
 class MainWindow(QtWidgets.QWidget):
@@ -26,8 +28,12 @@ class MainWindow(QtWidgets.QWidget):
         self.school_container.addWidget(self.school)
         self.search_container.addLayout(self.school_container)
 
+        today = datetime.datetime.now()
+
         self.year_container = QtWidgets.QHBoxLayout()
-        self.year = QtWidgets.QLineEdit()
+        self.year = QtWidgets.QSpinBox()
+        self.year.setRange(2019, today.year)
+        self.year.setValue(today.year)
         self.year_label = QtWidgets.QLabel()
         self.year_label.setText('년')
         self.year_label.setFont(QtGui.QFont('', 13))
@@ -37,7 +43,9 @@ class MainWindow(QtWidgets.QWidget):
         self.search_container.addLayout(self.year_container)
 
         self.month_container = QtWidgets.QHBoxLayout()
-        self.month = QtWidgets.QLineEdit()
+        self.month = QtWidgets.QSpinBox()
+        self.month.setRange(1, 12)
+        self.month.setValue(today.month)
         self.month_label = QtWidgets.QLabel()
         self.month_label.setText('월')
         self.month_label.setFont(QtGui.QFont('', 13))
@@ -48,6 +56,7 @@ class MainWindow(QtWidgets.QWidget):
 
         self.search_button = QtWidgets.QPushButton('검색')
         self.search_button.setFont(QtGui.QFont('', 13))
+        self.search_button.clicked.connect(self.search)
         self.search_container.addWidget(self.search_button)
         
         self.search_container.addStretch()
@@ -76,17 +85,26 @@ class MainWindow(QtWidgets.QWidget):
 
         self.container.addLayout(self.date_navigator_container)
 
-        self.calendar_container = QtWidgets.QHBoxLayout()
+        self.search_result_container = QtWidgets.QStackedLayout()
 
         self.calendar = QtWidgets.QTableWidget()
         self.calendar.setColumnCount(7)
-        self.calendar.setRowCount(5)
+        self.calendar.setRowCount(6)
         self.calendar.setHorizontalHeaderLabels(['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'])
         self.calendar.verticalHeader().setVisible(False)
 
-        self.calendar_container.addWidget(self.calendar)
+        self.search_result_container.addWidget(self.calendar)
 
-        self.container.addLayout(self.calendar_container)
+        self.no_search_result = QtWidgets.QLabel()
+        self.no_search_result.setText('검색 결과가 없습니다\n날짜가 잘못되었는지 확인해보시거나\nDB를 업데이트해주세요')
+        self.no_search_result.setAlignment(QtCore.Qt.AlignCenter)
+        self.no_search_result.setFont(QtGui.QFont('', 20))
+
+        self.search_result_container.addWidget(self.no_search_result)
+
+        self.search_result_container.setCurrentIndex(0)
+
+        self.container.addLayout(self.search_result_container)
 
         self.setLayout(self.container)
         self.setWindowTitle('급식')
@@ -102,3 +120,48 @@ class MainWindow(QtWidgets.QWidget):
         self.refresh_db_button.setText('DB 업데이트')
         self.refresh_db_button.setEnabled(True)
         self.refresh_db_button.repaint()
+
+    def search(self):
+        self.search_button.setText('검색 중...')
+        self.search_button.setEnabled(False)
+        self.search_button.repaint()
+
+        data = tuple(self.school_data.get_db(schl_nm=(self.school.text(), '=')))
+        data = data[0] if len(data) else None
+        year = self.year.text()
+        month = self.month.text()
+
+        print(data)
+
+        if data is None:
+            self.search_result_container.setCurrentIndex(1)
+        else:
+            start_day, meal_data = self.meal.get_month(data[0], year, month)
+
+            row_count, i_end = divmod(start_day + len(meal_data), 7)
+            j_start = start_day
+
+            for i in range(7):
+                self.calendar.setColumnWidth(i, 200)
+
+            for i in range(row_count):
+                base_index = i * 7 - start_day
+
+                for j in range(j_start, 7):
+                    index = base_index + j
+                    self.calendar.setCellWidget(i, j, MealTableItem(str(index + 1), meal_data[index]['lunch'], meal_data[index]['dinner']))
+
+                j_start = 0
+
+            for i in range(0, i_end):
+                index = row_count * 7 - start_day + i
+
+                self.calendar.setCellWidget(row_count, i, MealTableItem(str(index + 1), meal_data[index]['lunch'], meal_data[index]['dinner']))
+
+            self.calendar.resizeRowsToContents()
+
+            self.search_result_container.setCurrentIndex(0)
+
+        self.search_button.setText('검색')
+        self.search_button.setEnabled(True)
+        self.search_button.repaint()
---
+++ meal_table_item.py
@@ -0,0 +1,54 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QWidget
+
+
+class MealTableItem(QtWidgets.QWidget):
+    def __init__(self, date, lunch, dinner):
+        super(MealTableItem, self).__init__()
+        self.container = QtWidgets.QVBoxLayout()
+
+        self.date_label = QtWidgets.QLabel(date)
+        self.date_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.container.addWidget(self.date_label)
+
+        self.container.addStretch()
+        
+        self.lunch_container = QtWidgets.QVBoxLayout()
+        self.lunch_menus = [QtWidgets.QVBoxLayout() for i in range(len(lunch) if lunch is not None else 0)]
+        self.lunch_alergies = [QtWidgets.QHBoxLayout() for i in range(len(lunch) if lunch is not None else 0)]
+        
+        for i in range(len(lunch) if lunch is not None else 0):
+            self.lunch_menus[i].addWidget(QtWidgets.QLabel(lunch[i][0]))
+            
+            if lunch[i] is not None:
+                for j in lunch[i][1]:
+                    self.lunch_alergies[i].addWidget(QtWidgets.QLabel(str(j)))
+
+                self.lunch_alergies[i].addStretch()
+                self.lunch_menus[i].addLayout(self.lunch_alergies[i])
+
+            self.lunch_container.addLayout(self.lunch_menus[i])
+
+        self.container.addLayout(self.lunch_container)
+
+        self.container.addSpacerItem(QtWidgets.QSpacerItem(20, 40))
+        
+        self.dinner_container = QtWidgets.QVBoxLayout()
+        self.dinner_menus = [QtWidgets.QVBoxLayout() for i in range(len(dinner) if dinner is not None else 0)]
+        self.dinner_alergies = [QtWidgets.QHBoxLayout() for i in range(len(dinner) if dinner is not None else 0)]
+
+        for i in range(len(dinner) if dinner is not None else 0):
+            self.dinner_menus[i].addWidget(QtWidgets.QLabel(dinner[i][0]))
+
+            if dinner[i] is not None:
+                for j in dinner[i][1]:
+                    self.dinner_alergies[i].addWidget(QtWidgets.QLabel(str(j)))
+
+                self.dinner_alergies[i].addStretch()
+                self.dinner_menus[i].addLayout(self.dinner_alergies[i])
+
+            self.dinner_container.addLayout(self.dinner_menus[i])
+
+        self.container.addLayout(self.dinner_container)
+
+        self.setLayout(self.container)
